83c36d68b36eed972247d14fb93463dba2517943
Atishfulzade <atishfulzade1234@gmail.com>
2025-03-02 12:04:26 +0530

    Refactor requestServer utility to improve variable naming for backend URL and base route

--- client/src/utils/requestServer.js
+++ client/src/utils/requestServer.js
@@ -3,12 +3,12 @@ import axios from "axios";
 const requestServer = async (path, data = {}) => {
   try {
     const token = localStorage.getItem("token");
-    const baseurl = import.meta.env.VITE_SERVER_URL;
-    const base_url = import.meta.env.VITE_BASE_ROUTE;
-    console.log(`${baseurl}${base_url}${path}`);
+    const backendURL = import.meta.env.VITE_SERVER_URL;
+    const baseURL = import.meta.env.VITE_BASE_ROUTE;
+    console.log(`${backendURL}${baseURL}${path}`);
 
     const res = await axios({
-      url: `${baseurl}${base_url}${path}`,
+      url: `${backendURL}${baseURL}${path}`,
       method: "POST",
       data: Object.keys(data).length ? data : undefined,
       headers: {
