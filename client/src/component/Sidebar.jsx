99d4dd917aad809b4ab7033806df8a1120a7662d
Atishfulzade <atishfulzade1234@gmail.com>
2025-04-04 17:35:40 +0530

    Fix import path for SidebarContent component to include file extension

--- client/src/component/Sidebar.jsx
+++ client/src/component/Sidebar.jsx
@@ -36,7 +36,7 @@ import SidebarHeader from "./SidebarHeader";
 import SidebarFooter from "./SidebarFooter";
 import ContextMenu from "./ContextMenu";
 import { useNavigate } from "react-router-dom";
-import SidebarContent from "./SidebarContent";
+import SidebarContent from "./SidebarContent.jsx";
 
 const Sidebar = ({ onCollapse }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
